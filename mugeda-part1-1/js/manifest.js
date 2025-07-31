a7b6816afc1c16f46904553903259a4fe4a1c376
fish929Shen <zhoucharlie525@gmail.com>
2025-07-31 14:36:35 +0800

    Add files via upload

---
+++ mugeda-part1-1/js/manifest.js
@@ -0,0 +1,16 @@
+(function () {  
+
+_mrmcp = (typeof _mrmcp == 'undefined') ? {} : _mrmcp;
+_mrmcp['campaign_id'] = 'none';
+_mrmcp['owner_id'] = '67bad56a277e1602640a05cd';
+_mrmcp['creative_id'] = '6883bf205ec92f3275050e5c';
+_mrmcp['ga_url'] = _mrmcp['ga_url'] || (('https:' == document.location.protocol ? 'https://' : 'http://') + 'card.mugeda.com/weixin/card/ga.js');
+_mrmcp['width'] = _mrmcp['width'] || 1280;
+_mrmcp['height'] = _mrmcp['height'] || 720;
+_mrmcp['type'] = 'smart';
+_mrmcp['title'] = '数据新闻啊';
+_mrmcp['build_number'] = parseInt('460');
+_mrmcp['publish_time'] = 'Wed Jul 30 2025 05:12:25 GMT+0800 (China Standard Time)';
+_mrmcp['track_bot'] = 'http://cdn.mugeda.com/media/pages/track/track_20131030.html';
+_mrmcp['data_server'] = "https://weika.mugeda.cn";
+})();
